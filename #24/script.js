a42964b870a771656f73d1148082b4fc236b12d8
Alikhan <chort445@gmail.com>
2024-10-31 16:19:18 +0300

    first commit

---
+++ #24/script.js
@@ -0,0 +1,125 @@
+// let x = 60; косу
+// let y = 55;
+
+// let z = x + y;
+
+// console.log(z);
+
+
+
+// let a = 50; азайту
+// let b = 55;
+
+// console.log(a - b);
+
+
+
+
+// let a = 40;
+// let b = 10;
+
+// console.log(a * b);
+
+
+
+
+
+// let c = 400;
+// let d = 10;
+
+// console.log(c / d);
+
+
+
+
+// let num1 = 60;
+// let num2 = 20;
+
+// console.log(num1 % num2);
+
+
+
+
+// let num1 = 67;
+
+// if (num1 % 2 == 0){
+//     console.log("Jup san");
+// } else{
+//     console.log("Taq san");
+    
+// }
+
+
+
+
+
+
+// let num = 20;
+
+// num++;
+
+// console.log(num);
+
+// num--;
+
+// console.log(num);
+
+
+
+
+// let num = 20;
+
+// num += 5;
+
+// console.log(num);
+
+
+
+
+// let x = 5
+
+// x *= 2;
+// console.log(x);
+
+
+
+
+// let a = 20;
+
+// let b = 2;
+
+// let c = 60;
+
+// let d = 30;
+
+// let e = 2;
+
+
+// console.log(total = (a + b) * c - d / e );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a = 200;
+
+let b = 2;
+
+let c = 6;
+
+let z = 4;
+
+
+console.log(res = ((a * b) / c) /( z - 3));
+
+
+
